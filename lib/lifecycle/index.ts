be8a080f5da0c041fc67c0ce2a9f7b33f40aa23f
Yukikaze924 <yukikaze924@gmail.com>
2025-02-19 00:41:20 +0800

    chore: deprecate singleton function
    BREAKING CHANGE: The singleton function is deprecated and will be
    removed in future versions.

--- lib/lifecycle/index.ts
+++ lib/lifecycle/index.ts
@@ -1,4 +1,11 @@
-// Singleton function to ensure only one db instance is created
+/**
+ * @deprecated This function is deprecated and will be removed in future versions.
+ * Use the IoC Service Provider instead.
+ *
+ * @param name - The name of the singleton instance.
+ * @param value - A function that returns the singleton instance.
+ * @returns The singleton instance.
+ */
 export function singleton<T>(name: string, value: () => T): T {
     const globalAny: any = global;
     globalAny.__singletons = globalAny.__singletons || {};
